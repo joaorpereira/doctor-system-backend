137b7129c03d1a1345a02a49af0cb32ac03a4e28
joao <jprp17@live.com>
2021-05-11 23:59:23 -0300

    create new routes for new controller services

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,18 @@
 import express from 'express'
 import CompaniesController from '../controllers/CompaniesController'
+import ServicesController from '../controllers/ServicesController'
 
 const router = express.Router()
 
+// === company ===
 router.post('/company', CompaniesController.create)
 router.get('/company', CompaniesController.getCompanyList)
 router.get('/company/:id', CompaniesController.getCompany)
 router.put('/company/:id', CompaniesController.update)
+router.delete('/company/:id', CompaniesController.delete)
+
+// === service ===
+router.post('/service', ServicesController.create)
+router.get('/service/:id', ServicesController.getServicesList)
 
 export default router

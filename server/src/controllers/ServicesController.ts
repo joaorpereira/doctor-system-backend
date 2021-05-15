68090f4c453decc1c8c9970cc26ccd5ed80106d0
joao <jprp17@live.com>
2021-05-15 16:04:17 -0300

    feature: create methods getServices, update and removeInactiveService at Services Controller

--- server/src/controllers/ServicesController.ts
+++ server/src/controllers/ServicesController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express'
 import { ServicesModel } from '../models/services/services-model'
-import { Status, IServices } from '../models/services/services-types'
+import { Status } from '../models/services/services-types'
 import { FilesModel } from '../models/files/files-model'
 import Busboy from 'busboy'
-import { uploadToS3, deleteFileS3 } from '../services/aws'
+import { uploadToS3 } from '../services/aws'
 
 interface IBusboyRequest extends Request {
   files: any
@@ -15,6 +15,35 @@ interface IErrorAWS {
 
 class ServicesController {
   async getServicesList(req: Request, res: Response) {
+    try {
+      let newServices = []
+      const { id } = req.params
+
+      const services = await ServicesModel.find({
+        company_id: id,
+        status: { $ne: Status['REMOVIDO']},
+      })
+
+      for( let service of services){
+        const files = await FilesModel.find({
+          model: 'Services',
+          reference_id: service._id
+        })
+        newServices.push({
+          service: service._doc ,
+          files
+        })
+      }
+
+      res.status(200).send({
+        services: newServices,
+      })
+    } catch (error) {
+      res.status(404).send({ message: error.message })
+    }
+  }
+
+  async getFilteredServicesList(req: Request, res: Response) {
     try {
       const { id } = req.params
       const status: Status = Status['ATIVO']
@@ -51,7 +80,7 @@ class ServicesController {
         if (documentFile && objectKeysLength > 0) {
           for (let key of Object.keys(documentFile)) {
             const file = documentFile[key]
-            const nameParts : string[] = file.name.split('.')
+            const nameParts: string[] = file.name.split('.')
             const fileName = `${currentTime}.${nameParts[nameParts.length - 1]}`
             const path = `services/${company_id}/${fileName}`
 
@@ -72,7 +101,7 @@ class ServicesController {
           reference_id: newService._id as string,
           model: 'Services',
           folder: file,
-          created_at: new Date()
+          created_at: new Date(),
         }))
 
         await FilesModel.insertMany(newFiles)
@@ -84,6 +113,86 @@ class ServicesController {
         })
       })
       req.pipe(busboy)
+    } catch (error) {
+      res.status(404).send({ message: error.message })
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    const { company_id, service } = req.body
+    const { id } = req.params
+
+    const documentFile = (req as IBusboyRequest).files
+    const currentTime = new Date().getTime()
+
+    let errors: IErrorAWS[] = []
+    let files: string[] = []
+    let busboy = new Busboy({ headers: req.headers })
+
+    try {
+      busboy.on('finish', async () => {
+        const objectKeysLength = Object.keys(documentFile).length as number
+
+        if (documentFile && objectKeysLength > 0) {
+          for (let key of Object.keys(documentFile)) {
+            const file = documentFile[key]
+            const nameParts: string[] = file.name.split('.')
+            const fileName = `${currentTime}.${nameParts[nameParts.length - 1]}`
+            const path = `services/${company_id}/${fileName}`
+
+            const response = (await uploadToS3(file, path)) as any
+            if (response.error) {
+              errors.push({ error: response.error })
+            } else files.push(path)
+          }
+        }
+
+        if (errors.length > 0) {
+          return res.status(404).send(errors[0])
+        }
+
+        const jsonService = JSON.parse(service)
+        const newService = await ServicesModel.findByIdAndUpdate(
+          id,
+          jsonService
+        )
+
+        const newFiles = files.map(file => ({
+          reference_id: id,
+          model: 'Services',
+          folder: file,
+          created_at: new Date(),
+        }))
+
+        await FilesModel.insertMany(newFiles)
+
+        res.status(200).send({
+          files: newFiles,
+          service: newService,
+          message: 'Serviço atualizado com sucesso',
+        })
+      })
+      req.pipe(busboy)
+    } catch (error) {
+      res.status(404).send({ message: error.message })
+    }
+  }
+
+  async removeInactiveService(req: Request, res: Response) {
+    try {
+      const { id, status } = req.params
+
+      const newStatus: Status = status as Status
+
+      await ServicesModel.findByIdAndUpdate(id, {
+        status: Status[newStatus],
+      })
+
+      res
+        .status(200)
+        .send({
+          message: `Status do serviço atualizado com sucesso. Novo status: ${newStatus}`,
+        })
     } catch (error) {
       res.status(404).send({ message: error.message })
     }

309a418d98e249279249ee3bf9a413f2e26a2c51
joao <jprp17@live.com>
2021-05-15 16:04:40 -0300

    feature: add new controllers into routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import CompaniesController from '../controllers/CompaniesController'
+import FilesController from '../controllers/FilesController'
 import ServicesController from '../controllers/ServicesController'
 
 const router = express.Router()
@@ -7,12 +8,18 @@ const router = express.Router()
 // === company ===
 router.post('/company', CompaniesController.create)
 router.get('/company', CompaniesController.getCompanyList)
-router.get('/company/:id', CompaniesController.getCompany)
+router.get('/company/:id/:lat/:lon', CompaniesController.getCompany)
 router.put('/company/:id', CompaniesController.update)
 router.delete('/company/:id', CompaniesController.delete)
 
 // === service ===
 router.post('/service', ServicesController.uploadAWS)
 router.get('/service/:id', ServicesController.getServicesList)
+router.get('/service/filter/:id', ServicesController.getFilteredServicesList)
+router.put('/service/:id', ServicesController.update)
+router.delete('/service/:id/:status', ServicesController.removeInactiveService)
+
+// === file ===
+router.post('/file', FilesController.delete)
 
 export default router

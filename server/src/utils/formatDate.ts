44450253c6ad4164d4ee0f0cd67cc101ea7c2ef0
joao <jprp17@live.com>
2021-06-02 00:40:11 -0300

    feature: create a utils folder and formatDate file with helpers functions

---
+++ server/src/utils/formatDate.ts
@@ -0,0 +1,31 @@
+import { eachMinuteOfInterval, addMinutes } from 'date-fns'
+
+export const DURATION_TIME = 30
+
+export const timeConvert = (num: number, duration: number = 30) => {
+  const hours = Math.floor(num / 60)
+  const minutes = num % 60
+  return `${hours}:${minutes !== duration ? '00' : minutes}`
+}
+
+export const convertHourToMinutes = (date: Date) => {
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  return minutes + hour * 60
+}
+
+export const getIntervalByMinutes = (
+  startDate: Date,
+  endDate: Date,
+  totalMinutes?: number
+) => {
+  return eachMinuteOfInterval(
+    {
+      start: new Date(startDate),
+      end: totalMinutes
+        ? addMinutes(new Date(endDate), totalMinutes)
+        : new Date(endDate),
+    },
+    { step: 30 }
+  )
+}
